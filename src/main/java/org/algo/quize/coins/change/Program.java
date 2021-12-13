cb036bc60820c356f20f0ebc775868561c80319c
gpawel <gpawel@gmail.com>
2021-12-12 20:41:36 -0500

    still debugging

--- src/main/java/org/algo/quize/coins/change/Program.java
+++ src/main/java/org/algo/quize/coins/change/Program.java
@@ -27,7 +27,6 @@ class Program {
             for (int[] p : combination) {
                 builder.append("<" + p[0] + "*" + p[1] + "> ");
             }
-            builder.append("\n");
             return builder.toString();
         }
 
@@ -38,16 +37,15 @@ class Program {
 
     static int deepness = 0;
     static ArrayList<Combination> combinations = new ArrayList<>();
-    static Combination comb;
-    Combination comb = new Combination();
-    public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+    static Combination globalComb;
 
+    public static int numberOfWaysToMakeChange(int n, int[] denoms) {
         Arrays.sort(denoms);
         if (denoms[0] > n) throw new RuntimeException("input is invalid");
         for (int i = 0; i < denoms.length; i++) {
-            comb = new Combination();
-            searchCombinations(n,denoms, i);
-            combinations.add(comb);
+            globalComb = new Combination();
+            globalComb.addPortion(searchCombinations(n,denoms, i));
+            combinations.add(globalComb);
         }
         printCombinations(combinations);
         return combinations.size();
@@ -55,12 +53,15 @@ class Program {
 
     private static int[] searchCombinations(int n, int[] denoms, int currentCoinIndex) {
         deepness++;
-
+        Combination comb = new Combination();
         int coin = denoms[currentCoinIndex];
         int reminder = n % coin;
         int wholePart = n / coin;
         if (reminder == 0) {
             if (currentCoinIndex > 0 ) {
+                comb.addPortion(new int[] {coin,wholePart});
+                combinations.add(comb);
+                comb = new Combination();
                 for (int k=1; k<wholePart; k++) {
                     comb.addPortion(searchCombinations(n-k*coin, denoms,currentCoinIndex-1));
                 }
@@ -74,19 +75,19 @@ class Program {
                 throw new RuntimeException("Cannot build coins combination");
             }
         }
-        return new int[] {coin,wholePart}
         deepness--;
+        return new int[] {coin,wholePart};
+
     }
 
 
 
 
     private static void printCombinations(ArrayList<Combination> comb) {
-        StringBuilder b = new StringBuilder();
-        comb.stream().forEach(c -> {
-            b.append(c.toString());
-        });
-        System.out.println(b.toString());
+        for (int i=0; i<comb.size(); i++) {
+            System.out.println(comb.get(i).toString());
+        }
+
     }
 
 
